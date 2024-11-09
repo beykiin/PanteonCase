b4a0fdf522d1fe115e86885867c8a0af32740a50
Yasin Beken <yasinbeken16@gmail.com>
2024-11-10 00:27:31 +0300

    (Scripts) Added Collect Coin AI Script

---
+++ Assets/_GAME/Scripts/CollectCoinAI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoinAI : MonoBehaviour
+{
+    public Waypoint linkedWaypoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("AI"))
+        { 
+            Destroy(linkedWaypoint.gameObject);
+            linkedWaypoint = null;
+
+
+            other.GetComponent<AICharaters>().ChooseNextWaypoint();
+
+        }
+    }
+}
