10978a481503f170e3b48374f0185d694c2402ef
Yasin Beken <yasinbeken16@gmail.com>
2024-11-10 17:32:29 +0300

    (Script) Added Coin Lost Script

---
+++ Assets/_GAME/Scripts/CoinLostAI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CoinLostAI : MonoBehaviour
+{
+    [SerializeField] LayerMask aiLayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (IsAICollider(other))
+        {
+            Destroy(gameObject);
+        }
+    }
+    private bool IsAICollider(Collider collider) {
+
+        return ((aiLayer.value & (1 << collider.gameObject.layer)) != 0);
+
+    }
+        
+ 
+}
--- Assets/_GAME/Scripts/ThrowingUp.cs
+++ Assets/_GAME/Scripts/ThrowingUp.cs
@@ -6,17 +6,20 @@ public class ThrowingUp : MonoBehaviour, IInteractable
     [SerializeField] private float jumpForce = 4f;
     [SerializeField] private float duration = 1f;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private LayerMask aiLayer;
+
 
 
     private bool isThrowing = false;
     private float jumpStartTime = 0f;
-    private float yPos;
+    private float playerPos;
+    private float aiPos;
     private float jumpHeight = 0f;
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(IsPlayerLayer(collision.gameObject) && isThrowing)
+        if(IsPlayerLayer(collision.gameObject) && isThrowing || IsAILayer(collision.gameObject) && isThrowing)
         {
             IInteractable interactable = collision.gameObject.GetComponent<IInteractable>();
             if (interactable != null)
@@ -29,16 +32,37 @@ public class ThrowingUp : MonoBehaviour, IInteractable
     public void Interact()
     {
         var player = GameObject.FindWithTag("Player");
+        var ai = GameObject.FindWithTag("AI");
+
         if(player != null)
         {
-            yPos = player.transform.position.y;
+            playerPos = player.transform.position.y;
+            
+
             isThrowing= true;
             jumpStartTime= Time.time;
             jumpHeight= 0f;
-            Vector3 newPosition = player.transform.position;
-            newPosition.y+= jumpForce;
-            newPosition.z += 0.1f;
-            player.transform.position = newPosition;
+
+            Vector3 newPlayerPosition = player.transform.position;
+            newPlayerPosition.y+= jumpForce;
+            newPlayerPosition.z += 0.1f;
+            player.transform.position = newPlayerPosition;
+
+            
+        }
+
+        if(ai != null)
+        {
+            aiPos = ai.transform.position.y;
+
+            isThrowing = true;
+            jumpStartTime = Time.time;
+            jumpHeight = 0f;
+
+            Vector3 newAiPosition = ai.transform.position;
+            newAiPosition.y += jumpForce;
+            newAiPosition.z += 0.1f;
+            ai.transform.position = newAiPosition;
         }
     }
 
@@ -47,12 +71,31 @@ public class ThrowingUp : MonoBehaviour, IInteractable
         if (isThrowing)
         {
             var player = GameObject.FindWithTag("Player");
-            if(player != null)
+            var ai = GameObject.FindWithTag("AI");
+
+            if (player != null)
             {
                 float timeElapsed = Time.time - jumpStartTime;
 
                 jumpHeight = Mathf.Lerp(0f, jumpForce, timeElapsed / duration);
-                player.transform.position = new Vector3(player.transform.position.x, yPos + jumpHeight, player.transform.position.z);
+
+                player.transform.position = new Vector3(player.transform.position.x, playerPos + jumpHeight, player.transform.position.z);
+                
+
+                if (timeElapsed >= duration)
+                {
+                    isThrowing= false;
+                }
+            }
+
+            if (ai != null)
+            {
+                float timeElapsed = Time.time - jumpStartTime;
+
+                jumpHeight = Mathf.Lerp(0f, jumpForce, timeElapsed / duration);
+
+                
+                ai.transform.position = new Vector3(ai.transform.position.x, aiPos + jumpHeight, ai.transform.position.z);
 
                 if (timeElapsed >= duration)
                 {
@@ -65,5 +108,9 @@ public class ThrowingUp : MonoBehaviour, IInteractable
     private bool IsPlayerLayer(GameObject collision)
     {
         return ((1 << collision.gameObject.layer) & playerLayer) != 0;
+    }
+    private bool IsAILayer(GameObject collision)
+    {
+        return ((1 << collision.gameObject.layer) & aiLayer) != 0;
     }
 }
